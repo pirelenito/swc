09e979dcae80a290dde058c408fb37548a7a6ae9
Paulo Ragonha <paulo@ragonha.me>
2022-05-18 16:00:11 +0200

    Add unit test to detect exportNames including local variables

--- crates/swc_ecma_transforms_module/tests/common_js.rs
+++ crates/swc_ecma_transforms_module/tests/common_js.rs
@@ -5422,6 +5422,46 @@ exports.format = format = '123';
 "#
 );
 
+test!(
+    syntax(),
+    |_| tr(Config {
+        ..Default::default()
+    }),
+    export_names_should_not_include_local_variables,
+    "
+export * from 'another-module'
+
+export function whatever(notExportName) {
+  const shouldNotBeExportNameAsWell = 123
+  return shouldNotBeExportNameAsWell + notExportName
+}
+",
+    r#"
+"use strict";
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+var _exportNames = {};
+exports.whatever = whatever;
+var _anotherModule = require("another-module");
+Object.keys(_anotherModule).forEach(function(key) {
+    if (key === "default" || key === "__esModule") return;
+    if (Object.prototype.hasOwnProperty.call(_exportNames, key)) return;
+    if (key in exports && exports[key] === _anotherModule[key]) return;
+    Object.defineProperty(exports, key, {
+        enumerable: true,
+        get: function() {
+            return _anotherModule[key];
+        }
+    });
+});
+function whatever(notExportName) {
+    const shouldNotBeExportNameAsWell = 123;
+    return shouldNotBeExportNameAsWell + notExportName;
+}
+"#
+);
+
 #[testing::fixture("tests/fixture/commonjs/**/input.js")]
 fn fixture(input: PathBuf) {
     let dir = input.parent().unwrap().to_path_buf();
